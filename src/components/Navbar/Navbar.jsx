20288fc7a369b199bf2154c5d6fca6ee2324b0cc
Byron Keet <keetbis@gmail.com>
2022-12-14 14:53:07 +0200

    Added desktop navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
+import logo from '../../assets/logo.png';
 import './Navbar.scss';
 
 const Navbar = () => {
-  return (
-	<div>Navbar</div>
-  )
+	return (
+		<nav className='app__navbar'>
+			<div className='app__navbar-logo'>
+				<img src={logo} alt='logo' />
+			</div>
+			<ul className='app__navbar-links'>
+				{['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+					<li className='app__flex p-text' key={`link-${item}`}>
+						<div />
+						<a href={`#${item}`}>{item}</a>
+					</li>
+				))}
+			</ul>
+		</nav>
+	)
 }
 
 export default Navbar;
