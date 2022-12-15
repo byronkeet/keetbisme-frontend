d4fe87450bf3a8023dfb16e107b410aca06aab88
Byron Keet <keetbis@gmail.com>
2022-12-15 14:13:25 +0200

    Created app wrapper to wrap comonents with social media and nav dots

---
+++ src/components/NavigationDots.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/anchor-has-content */
+
+import React from 'react';
+
+const NavigationDots = ({ active }) => {
+	return (
+		<div className='app__navigation'>
+			{['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, i) => (
+				<a
+					href={`#${item}`}
+					key={item + i}
+					className='app__navigation-dot'
+					style={active === item ? { backgroundColor: '#313BAC' } : {} }
+				/>
+			))}
+		</div>
+	)
+}
+
+export default NavigationDots;
